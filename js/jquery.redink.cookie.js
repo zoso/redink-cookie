a9e23b35039bf445f26bcb07a8e5a26d0c6e60e5
Lars Lund <larslund@gmail.com>
2013-03-13 15:22:05 +0100

    v0.5

--- js/jquery.redink.cookie.js
+++ js/jquery.redink.cookie.js
@@ -1,65 +1,42 @@
 (function($){
     
-    //match cookie name
-    //document.cookie.match( name + "=([^;]*)(;|$)" );
-    //$.parseJSON(results[1]);
-    var dataArr = [];
-    var cookieObj = {};
-    var date = new Date();
-    // Get current date add/remove days "til expired
-    var days = 1;
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "<br>expire: "+date.toUTCString();
-    //var ipAddress = window.location.hostname.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/g);
-    var domainName = "." + window.location.hostname;
-    var options = {};
-    var text = expires+" <br>"+domainName;
-
-    //JSON.stringify(data)
-    //JSON.parse(cookie)
-
-    //decodeURIComponent
-    //encodeURIComponent
-
     /*
-    var obj = JSON.parse(document.cookie);
-    //console.log(obj.data.length);
+    usage:
+        - create array of pages to send to pdfcrowd
+        - cookie name
+        - expires?
+        - item:
+            page = {
+                url: "path/to/page",
+                title: "Title of Page",
+                img; "path/to/page", (same as url?)
+                extra: "some parameters" -> could be array
+            }
 
-    //add some data
-    var tmpArr = obj.data;
-    tmpArr.push({
-        data: tmpArr.length+1
-    });
-    obj.data = tmpArr;
-    var c = JSON.stringify(obj);
-    console.log(c);
-    document.cookie = c;
+    example:
+        - cookie:
+            {
+                name: "some name here",
+                version: 1,
+                expire: "data expires",
+                data: [
+                    {
+                        url: "path",
+                        title: "title",
+                        img: ?,
+                    {
+                        url: "another page",
+                        title: "my other page",
+                        img: "?",
+                        extra: "a simple string here"
+                    }
+                ],
+                extra: "" 
+            } //end cookie
+    
     */
-
-    /*
-
-     var cookieObj = {
-                name: 'the Cookie name',
-                version: 'the version',
-                expire: 0, //when user close browser
-                domain: "",
-                    path: "/",
-                    secure: false,
-                    data: [
-                        {
-                            "data": 1
-                        },
-                        {
-                            "data": 2
-                        }
-                    ],
-                    extra: "hello" //extra is where you can add whatever you want
-                }
-
-                var cookie = JSON.stringify(cookieObj);
-                document.cookie = cookie;
-
-    */
+    
+    //Default settings
     var defaultSettings = {
         name: "RedinkCookie",
         version: 1,
@@ -69,12 +46,24 @@
         extra: ""
     };
 
-    Array.prototype.remove = function(from, to) {
-        var rest = this.slice((to || from) + 1 || this.length);
-        this.length = from < 0 ? this.length + from : from;
-        return this.push.apply(this, rest);
+    //sorting the array
+    Array.prototype.move = function (old_index, new_index) {
+        while (old_index < 0) {
+            old_index += this.length;
+        }
+        while (new_index < 0) {
+            new_index += this.length;
+        }
+        if (new_index >= this.length) {
+            var k = new_index - this.length;
+            while ((k--) + 1) {
+                this.push(undefined);
+            }
+        }
+        this.splice(new_index, 0, this.splice(old_index, 1)[0]);
     };
 
+    //create the cookie
     $.RedinkCookie = function(options) {
         var res;
         if ($.RedinkCookie.check()) {
@@ -110,33 +99,6 @@
             res = "no data found";
         }
         return res;
-    }
-
-    $.RedinkCookie.removeData = function(nr) { //nr is the datas place in array...
-        var res;
-        if ($.RedinkCookie.check()) {
-            var obj = JSON.parse(document.cookie);
-            if (obj.hasOwnProperty("data")) {
-                for (var i = 0; i < obj.data.length; i++) {
-                    if (parseInt(nr) == i) {
-                        obj.data.splice(i,1);
-                        res = "deleted";
-                        break;
-                    }
-                }
-                document.cookie = JSON.stringify(obj);
-            } else {
-
-            }
-
-        } else {
-
-        }
-        return res;
-    }
-
-    $.RedinkCookie.sortData = function(arr) {
-        
     }
 
     $.RedinkCookie.getExtra = function() {
@@ -175,61 +137,52 @@
         return res;
     }
 
-    $.RedinkCookie.create = function(options) {
-        document.cookie = '{name: '+options.name+'};';
+    $.RedinkCookie.removeData = function(nr) { //nr is the datas place in array...
+        var res;
+        if ($.RedinkCookie.check()) {
+            var obj = JSON.parse(document.cookie);
+            if (obj.hasOwnProperty("data")) {
+                for (var i = 0; i < obj.data.length; i++) {
+                    if (parseInt(nr) == i) {
+                        obj.data.splice(i,1);
+                        res = "deleted";
+                        break;
+                    }
+                }
+                document.cookie = JSON.stringify(obj);
+            } else {
+                res = "no array";
+            }
+        } else {
+            res = "no data found";
+        }
+        return res;
     }
+
+    $.RedinkCookie.sortData = function(old_index, new_index) {
+        var res;
+        if ($.RedinkCookie.check()) {
+            var obj = JSON.parse(document.cookie);
+            if (obj.hasOwnProperty("data")) {
+                var tmpArr = obj.data;
+                tmpArr.move(old_index, new_index);
+                obj.data = tmpArr;
+                document.cookie = JSON.stringify(obj);
+                res = "sorted data";
+            } else { res = "no array found"}
+        } else {
+            res = "no data found";
+        }
+        return res;
+    }
+
+    /*$.RedinkCookie.create = function(options) {
+        document.cookie = '{name: '+options.name+'};';
+    }*/
 
     $.RedinkCookie.delete = function(name) {
         //console.log("-> "+document.cookie);
         document.cookie = null;//'name="";-1; path=/';
         return "cookie deleted";
     }
-
-
-    /*
-        usage:
-            - create array of pages to send to pdfcrowd
-            - cookie name
-            - expires?
-            - item:
-                page = {
-                    url: "path/to/page",
-                    title: "Title of Page",
-                    img; "path/to/page", (same as url?)
-                    extra: "some parameters" -> could be array
-                }
-
-        example:
-            - cookie:
-                {
-                    cookiename: "some name here",
-                    cookiecreated: "date created",
-                    cookieexpire: "data expires",
-                    cookiedata: [
-                        {
-                            url: "path",
-                            title: "title",
-                            img: ?,
-                            extra: [
-                                {
-                                    myVar: "something",
-                                    myVar2: "something 2"
-                                },
-                                {
-                                    myVar: "something else",
-                                    myVar2: "something completly different"
-                                }
-                            ]
-                        },
-                        {
-                            url: "another page",
-                            title: "my other page",
-                            img: "?",
-                            extra: "a simple string here"
-                        }
-                    ]
-                } //end cookie
-    
-    */
-
 })(jQuery);
