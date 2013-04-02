eddd7988ac521e133e3defca36101d1db08efa6a
Lars Lund <larslund@gmail.com>
2013-04-02 11:48:16 +0200

    Removed array prototype move

--- js/jquery.redink.cookie.js
+++ js/jquery.redink.cookie.js
@@ -14,23 +14,6 @@
         expire: 0
     }
 
-    //sorting the array
-    Array.prototype.move = function (old_index, new_index) {
-        while (old_index < 0) {
-            old_index += this.length;
-        }
-        while (new_index < 0) {
-            new_index += this.length;
-        }
-        if (new_index >= this.length) {
-            var k = new_index - this.length;
-            while ((k--) + 1) {
-                this.push(undefined);
-            }
-        }
-        this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-    };
-
     //default 
     $.cookie.json = true;
     var c_name;
@@ -43,6 +26,24 @@
 
     $.RedinkCookie.setCookieName = function(name) {
         c_name = name;
+    }
+
+    //sorting the array
+    $.RedinkCookie.move = function(arr, old_index, new_index) {
+        while (old_index < 0) {
+            old_index += arr.length;
+        }
+        while (new_index < 0) {
+            new_index += arr.length;
+        }
+        if (new_index >= arr.length) {
+            var k = new_index - arr.length;
+            while ((k--) + 1) {
+                arr.push(undefined);
+            }
+        }
+        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+        return arr;
     }
 
     $.RedinkCookie.check = function(name) {
@@ -93,8 +94,8 @@
         var obj = $.cookie(c_name);
         var state = false;
         if (obj.hasOwnProperty("data")) {
-            var tmpArr = obj.data;
-            tmpArr.move(old_index, new_index);
+            var tmpArr = $.RedinkCookie.move(obj.data, old_index, new_index);
+            //tmpArr.move(old_index, new_index);
             defaultSettings = $.extend({}, defaultSettings, {data: tmpArr});
             $.cookie(c_name, defaultSettings, options);
             state = true;
